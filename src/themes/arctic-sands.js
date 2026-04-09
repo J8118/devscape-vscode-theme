ec5051fc50f7e7d3bcbad7390baadfdbcb167f0b
J8118 <JG@8118.capital>
2026-04-09 23:30:35 +0400

    feat: add Arctic Sands theme config

---
+++ src/themes/arctic-sands.js
@@ -0,0 +1,19 @@
+module.exports = {
+    id: 'arctic-sands',
+    label: 'Arctic Sands',
+    accent: '#00c8ff',
+    accentRgb: '0,200,255',
+    glowColor: 'rgba(0,200,255,0.28)',
+    borderColor: '#1a2332',
+    backgroundImage: 'dunes.jpg',
+    backgroundVideo: 'bg-video.mp4',
+    backgroundOpacity: 0.13,
+    overlayDarkness: 0.86,
+    dashboardBodyBg: 'transparent',
+    injectionMarkers: {
+        cssStart: '/*devscape-arctic-sands-css-start*/',
+        cssEnd: '/*devscape-arctic-sands-css-end*/',
+        jsStart: '/*devscape-arctic-sands-js-start*/',
+        jsEnd: '/*devscape-arctic-sands-js-end*/',
+    }
+};
